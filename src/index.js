300d8f7229c831b680f6a14e508ce6f63b11ad6c
Lucas Silva <lucas.silva@AD.AGENCIAENNOVE>
2021-11-10 17:01:25 -0400

    atividade

---
+++ src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+function getName(nome) {
+    return "Ol\u00E1 mundo " + nome;
+}
+console.log(getName('Lucas'));
---
+++ src/index.ts
@@ -0,0 +1,117 @@
+
+// atividade 1
+
+//   let varA = 10;
+// let varB = 20;
+// let varC = 0;
+
+// varC = varA;
+// varA = varB;
+// varB = varC;
+// console.log("A:", varA, "B:", varC);
+
+
+// atividade 2
+
+// let valorA = 20;
+// let valorB = 10;
+// if(valorA > valorB) {
+//   console.log ("o valor maior é:", valorA)
+// }else{
+//   console.log ("o valor maior é:", valorA)
+// }
+
+
+// atividade 3
+// let valorUm = 5;
+// let valorDois = 10;
+
+// if(valorUm < valorDois || valorDois < valorUm){
+//   console.log("valor menor:", valorUm, "valor maior:", valorDois);
+// }
+
+// // atividade 4
+// function cliente(){
+//   let saldo = 150;
+//   let debito = 200;
+//   let credito = 300;
+//   let saldoAtual = saldo - debito + credito;
+
+//   if(saldoAtual > 0) {
+//     console.log("saldo positivo");
+//   }else if(saldoAtual = 0) {
+//     console.log("saldo zerado");
+//   }else if (saldoAtual < 0) {
+//     console.log("saldo positivo");
+//   }else {
+//     console.log("negativo");
+//   }
+
+  
+//   return console.log(saldoAtual);
+// }
+
+// Atividade 5
+
+// function estoque(){
+//   let qtdMaxima = 200;
+//   let qtdMinima = 100;
+//   let qtdAtual = 600;
+//   let qtdMedia =  qtdMaxima + qtdMinima/2;
+
+//   if(qtdMedia >= qtdAtual){
+//     console.log("Quantidade média =",qtdMedia,".Não efetuar compra!");
+//   }else if(qtdMedia < qtdAtual) {
+//     console.log("Quantidade média =:",qtdMedia,".Não efetuar compra");
+//   }else{
+//     console.log(" ");
+//   }
+ 
+//   return qtdMedia;
+// }
+
+
+// // Atividade 6
+
+// let A = 0;
+
+// if(A > 0){
+//   console.log("valor positivo");
+// }else if(A < 0){
+//   console.log("valor negativo");
+// }else if(A == 0){
+//   console.log("valor zero");
+// }else{
+//   console.log("Zero");
+// }
+
+// console.log(A);
+
+// atividade 7
+
+// let A = 10;
+// let B = 20;
+// let C = 5;
+
+// if(A > B && A < C){
+//   console.log("O valor maior é: ", A);
+// }else if(B > A && B > C){
+//   console.log("O valor maior é: ", B);
+// }else if(C > A && C > B){
+//   console.log("O valor maior é: ",C);
+// }
+
+
+//atividade 8
+
+// let A = 10;
+// let B = 20;
+// let C = 50;
+
+// if(C < A && (A < B)){
+//   console.log("valor:",A+B);
+// }else if(B < A && (A < C)){
+//   console.log("valor:", A +C);
+// }else{
+//   console.log("valor:",B+C)
+// }
